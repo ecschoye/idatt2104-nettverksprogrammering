071b769e5eda726a5bef10a0a3f5eae65778db80
Edvard <ecschoye@stud.ntnu.no>
2023-02-13 20:02:24 +0100

    started p4

--- oblig-p4-java/src/task1/Client.java
+++ oblig-p4-java/src/task1/Client.java
@@ -1,4 +1,35 @@
 package task1;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        Scanner readerFromCommandLine = new Scanner(System.in);
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress address = InetAddress.getLocalHost();
+        byte[] buf = null;
+
+        while (true) {
+            System.out.println("Enter math expression: ");
+            System.out.println("Example: 2 + 2, 2 - 2, 2 * 2, 2 / 2");
+            System.out.println("Enter 'n' to exit");
+            String expression = readerFromCommandLine.nextLine();
+            buf = expression.getBytes();
+            DatagramPacket sendingPacket = new DatagramPacket(buf, buf.length, address, 1250);
+            socket.send(sendingPacket);
+
+            if (expression.equalsIgnoreCase("n")) {
+                break;
+            }
+
+            DatagramPacket receivingPacket = new DatagramPacket(buf, buf.length);
+            socket.receive(receivingPacket);
+            String received = new String(buf, 0, buf.length);
+            System.out.println("Result: " + received);
+
+        }
+        socket.close();
+    }
 }
--- oblig-p4-java/src/task1/Server.java
+++ oblig-p4-java/src/task1/Server.java
@@ -1,4 +1,48 @@
 package task1;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket(1250);
+        byte[] buf = new byte[256];
+        DatagramPacket receivePacket = null;
+        DatagramPacket responsePacket = null;
+        while (true) {
+            buf = new byte[256];
+
+            receivePacket = new DatagramPacket(buf, buf.length);
+            // Receive expression
+            socket.receive(receivePacket);
+            String expression = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            expression = expression.trim();
+            System.out.println("Received expression: " + expression);
+
+            if (expression.equalsIgnoreCase("n")) {
+                break;
+            }
+
+            // Perform calculation
+            String[] expressionParts = expression.split(" ");
+            int number1 = Integer.parseInt(expressionParts[0]);
+            int number2 = Integer.parseInt(expressionParts[2]);
+            String operator = expressionParts[1];
+            int result = 0;
+            switch (operator) {
+                case "+" -> result = number1 + number2;
+                case "-" -> result = number1 - number2;
+                case "*" -> result = number1 * number2;
+                case "/" -> result = number1 / number2;
+            }
+            buf = Integer.toString(result).getBytes();
+            int port = receivePacket.getPort();
+            responsePacket = new DatagramPacket(buf, buf.length, receivePacket.getAddress(), port);
+            socket.send(responsePacket);
+
+
+        }
+        socket.close();
+    }
 }
